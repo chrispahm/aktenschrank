42977a036166629ecc1e0ef56433c61840faf012
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2020-01-28 10:31:12 +0100

    use turf

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Gt06 = require('gt06');
 const net = require('net');
+const turf = require('@turf/turf')
 const credentials = require('./credentials')
 const nano = require('nano')(`http://${credentials.server.user}:${encodeURIComponent(credentials.server.password)}@localhost:5984`)
 const gps = nano.db.use('gps')
+
+const cache = {}
 
 var server = net.createServer((client) => {
   var gt06 = new Gt06();
@@ -21,7 +24,20 @@ var server = net.createServer((client) => {
     }
 
     if (gt06.imei && gt06.lat && gt06.lon) {
-      console.log(gt06.imei, gt06.lat, gt06.lon)
+      // check moving distance
+      if (!cache[imei]) cache[imei] = []
+      // store latest two locations, add the new location to the start of the array
+      cache[imei].unshift([gt06.lon, gt06.lat])
+      if (cache[imei].length === 4) {
+        // remove last location from array
+        cache[imei].pop()
+        // check the distance between the points
+        const first = turf.point(cache[imei][2])
+        const second = turf.point(cache[imei][0])
+        const distance = turf.distance(first,second) * 1000
+        console.log(distance)
+      }
+      //
       try {
         const timestamp = new Date().toISOString()
         await gps.insert({
