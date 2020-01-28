93ef516f4cda8cecb8da596e3c6661247a500a59
Christoph Pahmeyer <christoph.pahmeyer@ilr.uni-bonn.de>
2020-01-28 09:51:42 +0100

    use couch for now

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Gt06 = require('gt06');
 const net = require('net');
+const credentials = require('./credentials')
+const nano = require('nano')(`http://${credentials.server.user}:${encodeURIComponent(credentials.server.password)}@localhost:5984`)
+const gps = nano.db.use('gps')
 
-var server = net.createServer((client) => {
+var server = net.createServer(async (client) => {
   var gt06 = new Gt06();
   console.log('client connected');
 
@@ -10,7 +13,6 @@ var server = net.createServer((client) => {
       gt06.parse(data);
     }
     catch (e) {
-      console.log('err', e);
       return;
     }
 
@@ -20,14 +22,15 @@ var server = net.createServer((client) => {
 
     if (gt06.imei && gt06.lat && gt06.lon) {
       console.log(gt06.imei, gt06.lat, gt06.lon)
-      /* rethinkdb store 
-       {
-       imei: gt06.imei,
-       lat: gt06.lat
-       lng: gt06.lon,
-       time: new Date().toISOString()
-     }
-      */
+      try {
+        await gps.insert({
+          imei: gt06.imei,
+          pos: [gt06.lon, gt06.lat],
+          time: new Date().toISOString()
+        })
+      } catch (e) {
+        console.log('Couch Error: ' + e);
+      }
     }
 
     gt06.clearMsgBuffer();
